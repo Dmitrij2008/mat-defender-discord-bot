f0968289a95a1a1fd279d7452bfb0a914105ae20
lcomrade <70049256+lcomrade@users.noreply.github.com>
2021-01-11 19:52:39 +0700

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm /usr/bin/mat-defender-bot
+rm /usr/bin/mat-defender-bot-reconfigure
+
+rm /usr/share/dict/mat
+
+rm /lib/systemd/system/mat-defender-bot.service
+
+rm -rf /usr/share/doc/mat-defender-bot
